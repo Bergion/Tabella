32f7c5da922db74d022b915a1f9d9254745b6eb8
Bergion <dima.vintsyk@gmail.com>
2021-03-24 12:42:03 +0200

    configured grpc

--- src/Gateways/Web.HttpAggregator/Startup.cs
+++ src/Gateways/Web.HttpAggregator/Startup.cs
@@ -1,51 +1,36 @@
-using Cabinet.API.Infrastructure;
-using Cabinet.API.Managers;
-using Cabinet.API.Services;
-using Cabinet.API.Services.Abstractions;
-using Cabinet.Storage;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-namespace Cabinet.API
+namespace Web.HttpAggregator
 {
 	public class Startup
 	{
-		private readonly IWebHostEnvironment _enviroment;
-
-		public Startup(IConfiguration configuration,
-			IWebHostEnvironment enviroment)
+		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
-			_enviroment = enviroment;
 		}
 
 		public IConfiguration Configuration { get; }
 
-
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<CabinetContext>(options =>
-				options.UseSqlServer(Configuration.GetConnectionString("DevelopmentConnection")));
 
-			services.AddFileStorage(options =>
-			{
-				options.Configure(Configuration.GetSection("DevelopmentStorage").Get<StorageConfiguration>());
-			});
-
-			services.AddTransient<DocumentManager>();
-
-			services.AddGrpc(options => options.EnableDetailedErrors = true);
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cabinet.API", Version = "v1" });
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web.HttpAggregator", Version = "v1" });
 			});
 		}
 
@@ -56,8 +41,9 @@ namespace Cabinet.API
 			{
 				app.UseDeveloperExceptionPage();
 				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cabinet.API v1"));
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web.HttpAggregator v1"));
 			}
+
 			app.UseHttpsRedirection();
 
 			app.UseRouting();
