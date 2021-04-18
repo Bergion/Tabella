561a18c48bea94288f52d81cf773972038740a0b
Bergion <dima.vintsyk@gmail.com>
2021-04-18 10:28:16 +0300

    Added folder type

--- src/Microservices/Cabinet/Cabinet.API/Models/Folder.cs
+++ src/Microservices/Cabinet/Cabinet.API/Models/Folder.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cabinet.API.Models
 {
+	public enum FolderType
+	{
+		Default,
+		Incoming,
+		Outgoing
+	}
+
 	public class Folder
 	{
 		public int ID { get; set; }
 
 		public string Name { get; set; }
+
+		public FolderType Type { get; set; }
 
 		public int OrganizationID { get; set; }
 
