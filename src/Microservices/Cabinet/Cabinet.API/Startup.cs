69905a46ffbe8e7b72ccc5964d56290524af2fb6
Bergion <dima.vintsyk@gmail.com>
2021-04-21 18:28:05 +0300

    Front-end. Folder demo

--- src/Microservices/Cabinet/Cabinet.API/Startup.cs
+++ src/Microservices/Cabinet/Cabinet.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 
@@ -52,7 +53,7 @@ namespace Cabinet.API
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
 		{
 			if (env.IsDevelopment())
 			{
