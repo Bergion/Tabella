c5cd430b8dd9ec859ab40937ae9bf850d25d7272
Bergion <dima.vintsyk@gmail.com>
2021-04-21 18:28:51 +0300

    Revert "Front-end. Folder demo"
    This reverts commit 69905a46ffbe8e7b72ccc5964d56290524af2fb6.

--- src/Microservices/Cabinet/Cabinet.API/Startup.cs
+++ src/Microservices/Cabinet/Cabinet.API/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 
@@ -53,7 +52,7 @@ namespace Cabinet.API
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
